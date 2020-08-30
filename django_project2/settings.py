227f969200a0be92cd680fc63a7427b4baf7fa69
Jerzy <jgrynczewski@gmail.com>
2020-08-30 10:53:51 +0200

    cd part5

--- django_project2/settings.py
+++ django_project2/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-import django_heroku
-django_heroku.settings(locals())
+if os.getenv("DEPLOY"):
+    import django_heroku
+    django_heroku.settings(locals())
 
